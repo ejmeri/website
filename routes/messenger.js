5f6e281f62b9709c6186885df5f1c738fc9c42ac
ejmeri <elmerisilva@hotmail.com>
2018-03-27 17:46:35 -0300

    error messenger

--- routes/messenger.js
+++ routes/messenger.js
@@ -37,15 +37,15 @@ router.post('/webhook', async function (req, res) {
                         var payload = event.postback.payload;
 
                         if (payload == 'GET_STARTED_PAYLOAD')
-                            Messenger.prototype.sendTextMessage(event.sender.id, 'Olá, tudo bem? Para ver o status de alguma linha utitlize o menu :D');
+                            Messenger.sendTextMessage(event.sender.id, 'Olá, tudo bem? Para ver o status de alguma linha utitlize o menu :D');
                         else if (payload == 'HELP_PAYLOAD')
-                            Messenger.prototype.sendTextMessage(event.sender.id, 'Olá, sou o Metrozera :)\n\nPara ver o status de alguma linha utitlize o menu :D');
+                            Messenger.sendTextMessage(event.sender.id, 'Olá, sou o Metrozera :)\n\nPara ver o status de alguma linha utitlize o menu :D');
                         else {
                             if (payload == 'Lilas')
                                 payload = payload.replace('a', 'á');
 
                             payload = await Messenger.sendStatusLine(payload);
-                            Messenger.prototype.sendTextMessage(event.sender.id, payload);
+                            Messenger.sendTextMessage(event.sender.id, payload);
                         }
 
                     } else
